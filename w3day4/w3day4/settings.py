8d542372b08f9a90cd0829e9c409f5edc2eef30c
PriyanshuSahu2 <Sahupriyanshu108@gmail.com>
2024-05-02 08:33:08 +0530

    week 3 day 4 Added styles and html

--- w3day4/w3day4/settings.py
+++ w3day4/w3day4/settings.py
@@ -18,6 +18,7 @@ import myapp
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATE_DIR = os.path.join(BASE_DIR, 'template')
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
@@ -120,7 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATICFILES_DIRS = [STATIC_DIR]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
