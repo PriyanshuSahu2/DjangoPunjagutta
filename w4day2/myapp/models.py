3ce3811464c3bd3ddd73a68249d42f680ca6a276
PriyanshuSahu2 <Sahupriyanshu108@gmail.com>
2024-05-07 08:50:10 +0530

    week 4 Day 2 Models and Fields

---
+++ w4day2/myapp/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+
+class Student(models.Model):
+    name = models.CharField(max_length=25, null=True)
+    age = models.PositiveSmallIntegerField()
+    address = models.CharField(max_length=25, null=True)
